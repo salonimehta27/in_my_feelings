06c774198eb75b6ef9c097a625a49f9b30ec68f6
Saloni <salonimehta2127@gmail.com>
2021-11-27 17:54:53 -0500

    updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
 
     skip_before_action :authorize, only: :create
 
+
+    # def index
     def show
         user=User.find_by(id: session[:user_id])
         render json: user, status: :ok
