6190e2734e7e698af8a256b5b75d330e11e83a09
Saloni <salonimehta2127@gmail.com>
2021-12-15 21:31:46 -0500

    post changes

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import {Link,NavLink} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import {MdPassword} from 'react-icons/md'
 import {BsPersonFill,BsPersonLinesFill} from 'react-icons/bs'
 import PageSwitcher from './PageSwitcher'
