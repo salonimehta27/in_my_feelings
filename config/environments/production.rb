363e0e7b9103e9d958682b33205695bb4a94e6a0
Saloni <salonimehta2127@gmail.com>
2021-12-21 13:12:11 -0500

    Made url change in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://in-my-f33lings.herokuapp.com/cable'
+  config.action_cable.url = 'wss://localhost:3000/cable'
   config.action_cable.allowed_request_origins = [ 'http://in-my-f33lings.herokuapp.com', /http:\/\/in-my-f33lings.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
